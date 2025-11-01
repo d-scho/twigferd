77ba702598ee70733f3af37302eed423047ecd37
David Schönhofen <d.schoenhofen.dev@gmail.com>
2025-11-01 11:15:41 +0100

    Sketch out Zugferd creation

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Dompdf\Dompdf;
+use horstoeko\zugferd\codelists\ZugferdInvoiceType;
+use horstoeko\zugferd\ZugferdDocumentBuilder;
+use horstoeko\zugferd\ZugferdDocumentPdfBuilder;
+use horstoeko\zugferd\ZugferdProfiles;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Twig\Environment;
@@ -15,6 +20,38 @@ final readonly class IndexController
     {
         $html = $twig->render('index.html.twig');
 
-        return new Response($html);
+        $pdf = new Dompdf();
+        $pdf->loadHtml($html);
+        $pdf->setPaper('A4');
+        $pdf->render();
+
+        $xmlBuilder = ZugferdDocumentBuilder::createNew(ZugferdProfiles::PROFILE_BASIC);
+        $xmlBuilder->setDocumentInformation(
+            documentNo: 'documentNo',
+            documentTypeCode: ZugferdInvoiceType::INVOICE,
+            documentDate: new \DateTimeImmutable(),
+            invoiceCurrency: 'EUR',
+        );
+        $xmlBuilder->setDocumentSeller('seller');
+
+        $zugferdBuilder = new ZugferdDocumentPdfBuilder(
+            $xmlBuilder,
+            $pdf->output(),
+        );
+        $zugferdBuilder->generateDocument();
+
+        $fileName = 'some_file_name.pdf';
+
+        $binaryString = $zugferdBuilder->downloadString();
+
+        return new Response(
+            $binaryString,
+            Response::HTTP_OK,
+            [
+                'Content-Type' => 'application/pdf',
+                'Content-Disposition' => 'attachment; filename="'.$fileName.'"',
+                'Content-Length' => strlen($binaryString),
+            ],
+        );
     }
 }
